0c608aaf4d0ed437cb3b7440a84bb3af600917c6
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-02-09 06:33:38 +0100

    feat: fix err

--- src/components/feature-section/index.tsx
+++ src/components/feature-section/index.tsx
@@ -60,9 +60,7 @@ const FeatureSection = () => (
                 height="320"
                 src="https://www.youtube.com/embed/jT6yx2kTvFQ"
                 title="YouTube video player"
-                frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
               ></iframe>
               {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/jT6yx2kTvFQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
             </div>
