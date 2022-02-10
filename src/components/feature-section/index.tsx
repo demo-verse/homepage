0fc295edc0bf76afc5475c42bf818fd924495bc6
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-02-10 13:07:00 +0100

    feat: fix link

--- src/components/feature-section/index.tsx
+++ src/components/feature-section/index.tsx
@@ -133,11 +133,7 @@ const FeatureSection = () => (
                   `bg-red-600 text-2xl text-white active:bg-pink-600 hover:bg-black hover:text-yellow-400 font-bold lg:text-lg md:text-md px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-16 w-42 rounded-full`,
                 )}
               >
-                <a
-                  href="https://gateway.pinata.cloud/ipfs/QmYBM9RpQEV6jbYM3Wf6QsZgoeeAAeKHRo1dmvhGc9xgUs"
-                  target="_blank"
-                  referrerPolicy="no-referrer"
-                >
+                <a href="https://en.wikipedia.org/wiki/Blockchain" target="_blank" referrerPolicy="no-referrer">
                   ? blockchain
                 </a>
               </button>
